8062391487205dc5840a8e45c294bc5e34c83b72
oisheeeeera142-rgb <oisheeeeera142@gmail.com>
2026-01-18 13:09:52 +0600

    Update paymentmng.php

--- hotelreservationmng/admin/controller/paymentmng.php
+++ hotelreservationmng/admin/controller/paymentmng.php
@@ -1,24 +1,57 @@
+
+
+
+
 <?php
-$method = "";
-$status = ""; 
-$datetime = ""; 
+include "../model/paymentmodel.php";
+
+$success = "";
 $error = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-if (empty($_POST["method"]) || empty($_POST["status"]) || empty($_POST["datetime"])) 
-$error = "Please fill in all fields.";{
-else {
-$method   = $_POST["method"];
-$status   = $_POST["status"];
-$datetime = $_POST["datetime"];
+$editPayment = null;
+
+if (isset($_POST['add'])) {
+$booking_id = $_POST['booking_id'];
+$amount     = $_POST['amount'];
+$method     = $_POST['method'];
+$status     = $_POST['status'];
+$date       = $_POST['payment_date'];
+$time       = $_POST['payment_time'];
+if (empty($booking_id) || empty($amount) || empty($method) || empty($status) || empty($date) || empty($time)) {
+$error = "All fields are required!";
+} else {
+if (addPayment($conn, $booking_id, $amount, $method, $status, $date, $time)) {
+$success = "Payment added successfully!";
+} else {
+$error = mysqli_error($conn);
 }
 }
-if ($_SERVER["REQUEST_METHOD"] == "POST" && empty($error)) {
-echo "<h3>Input:</h3>";
-echo "Method: " . $method . "<br>";
-echo "Status: " . $status . "<br>";
-echo "DateTime: " . $datetime . "<br>";
 }
-?> 
+if (isset($_POST['update'])) {
+$id= $_POST['id']; 
+$booking_id = $_POST['booking_id'];
+$amount    = $_POST['amount'];
+$method    = $_POST['method'];
+$status    = $_POST['status'];
+$date      = $_POST['payment_date'];
+$time     = $_POST['payment_time'];
 
-
-
+if (updatePayment($conn, $id, $booking_id, $amount, $method, $status, $date, $time)) {
+$success = "Payment updated successfully!";
+} else {
+$error = mysqli_error($conn);
+}
+}
+if (isset($_GET['delete'])) {
+$id = (int)$_GET['delete'];
+if (deletePayment($conn, $id)) {
+$success = "Payment deleted successfully!";
+} else {
+$error = "Delete failed: " . mysqli_error($conn);
+}
+}
+if (isset($_GET['edit'])) {
+$id = (int)$_GET['edit'];
+$editPayment = getPaymentById($conn, $id);
+}
+$payments = getAllPayments($conn);
+?>
